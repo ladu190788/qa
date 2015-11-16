f6c04f66d5aab05554b5af62ee0e5f35e3580763
mano <mano@gmail.com>
2015-11-17 00:05:46 +0530

    1st commit

---
+++ test1/src/test/java/t1/test1/PrintTest.java
@@ -0,0 +1,16 @@
+package t1.test1;
+
+import org.testng.annotations.Test;
+
+
+public class PrintTest 
+{
+
+	@Test
+	public void printdata()
+	{
+		System.out.println("This is test testcase");
+	}
+	
+	
+}
